0f79126afabc5a3d95f870343e151a0a4bb0e360
jerrychen <jerrychen040@gmail.com>
2016-09-29 23:49:15 +0800

    add error page and try the link_to in html

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
+
+  #set root
+  root 'pages#home'
+
   #get 'pages/contact'
   get "contact", to: "pages#contact"
   #get 'pages/about'
   get "about", to: "pages#about"
-
+  get "error", to: "pages#error"
 
   resources :projects
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  #learn how to redirect the localhost:3000/blog  to yahoo
+  get 'blog', to:redirect("http://yahoo.com.tw")
+  # catch the *path, put near the end or the routes.rb
+  get '*path', to: redirect("/error")
 
-  root 'pages#home'
 end
