d44682d1e64a2d5b92068109d5ba61478f8ed3c0
jerrychen <jerrychen040@gmail.com>
2016-09-30 11:07:12 +0800

    add one more column in existing db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930023446) do
+ActiveRecord::Schema.define(version: 20160930030326) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160930023446) do
     t.integer  "project_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "completed"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
