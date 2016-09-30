252c980d5af7f1e4853eca559ab4be3e4328cfed
jerrychen <jerrychen040@gmail.com>
2016-09-30 10:22:12 +0800

    add db default values in Model (after_initialize)

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,10 +7,29 @@ class Project < ApplicationRecord
   #integrate callbacks
   #integrating database relationships
 ############################################
-  #Custom scopes
 
+  #############
+  # add defaults
+  ##############
+  # the method set_defaults we will add below
+  # and means when Object initialized, then it will run
+  # the set_defaults method, it is the timing to set some default value
+  # (Or you can set when you create the db)
+  # Don't use after_create, time is not right, will weap out the value you have.
+  after_initialize :set_defaults
+
+  ################
+  #Custom scopes #
+  ###############
   #this can be called from controller.
   scope :almost_completed, -> { where('percent_complete > 75.0')}
   scope :still_needs_some_work, -> { where('percent_complete < 75.0')}
+
+
+  def set_defaults
+    # || = , means, if the percent_complete has value , keep it.
+    # if set_defaults == nil, then set it to 0.0
+    self.percent_complete ||= 0.0
+  end
 
 end
